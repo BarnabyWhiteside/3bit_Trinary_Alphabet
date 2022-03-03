d33c7937119cc5dfe134d5391ccf24b7da84c1a2
Barnaby Whiteside <41023917+BarnabyWhiteside@users.noreply.github.com>
2022-03-03 14:02:46 +0000

    made small changes

--- 3bit.py
+++ 3bit.py
@@ -7,9 +7,9 @@ three_bit_trinary = [
 ]
 
 alphabet = [
-    'a','b','c','d','e','f','g','h','i',
-    'j','k','l','m','n','o','p','q','r',
-    's','t','u','v','w','x','y','z',' '
+    ' ','a','b','c','d','e','f','g','h',
+    'i','j','k','l','m','n','o','p','q',
+    'r','s','t','u','v','w','x','y','z'
 ]
 
 three_bit_output2 = ""
